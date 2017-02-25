0c0489ff2840784d1c7644689f9977d1883648cc
P250HEADSHOT <serg99spb@mail.ru>
2017-02-25 17:40:54 +0300

    black&whitelists

--- mobi.py
+++ mobi.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 # continuous-integration
 import telebot
-
+from pprint import pprint
 technoconf = -1001070076534
 me = 94026383
 TA= -1001109363260
 token='357359911:AAHxnKF-bXuVQKUVxsaV_FTqXSJg8AkbFDE'
-
 bot = telebot.TeleBot(token)
+twoch={
+    'test':-1001100823817
+}
+blacklist={
+    'TA':-1001109363260
+}
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     print(message)
     if "Ассистент, тест" in message.text:
             bot.send_message(me,'TESTED!')
+    for n in twoch:
+        print(twoch[n])
+        if not(message.chat.id == twoch[n]):
+            bot.send_message(message.chat.id,'Contact @Kylmakalle first!')
+            bot.leave_chat(message.chat.id)
 
 @bot.message_handler(content_types=['photo'])
 def handle_photos(message):
@@ -22,9 +33,11 @@ def handle_photos(message):
         if not (message.chat.type == 'private'):
             #Admins = bot.get_chat_administrators(message.chat.id)
             #for i in range(len(list(Admins))):
-                #print(Admins[i])
-                #if 'id' in Admins[i]['user'] != message.from_user.id:
-                    if (forwared == TA):
+             #   pprint(Admins)
+               # if 'id' in Admins[i]['user'] != message.from_user.id:
+                for k in blacklist:
+                    print(blacklist[k])
+                    if (forwared == blacklist[k]):
                         bot.kick_chat_member(message.chat.id,message.from_user.id)
                         bot.send_message(message.chat.id, message.from_user.first_name+'(@' + message.from_user.username + ') '+'BANNED!')
                         bot.forward_message(me,message.chat.id,message.message_id)
