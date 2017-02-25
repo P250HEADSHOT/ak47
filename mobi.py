e672fa077b0838a1d10db078d9ab4bff5893f16e
P250HEADSHOT <serg99spb@mail.ru>
2017-02-25 21:14:43 +0300

    redistest

--- mobi.py
+++ mobi.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # continuous-integration
 import telebot
+import os
+import redis
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis = redis.from_url(redis_url)
 from pprint import pprint
 technoconf = -1001070076534
 me = 94026383
@@ -8,22 +13,34 @@ TA= -1001109363260
 token='357359911:AAHxnKF-bXuVQKUVxsaV_FTqXSJg8AkbFDE'
 bot = telebot.TeleBot(token)
 twoch={
+    '@Kylmakalle':94026383,
     'test':-1001100823817
 }
 blacklist={
     'TA':-1001109363260
 }
-
+r = redis.from_url(os.environ.get("REDIS_URL"))
+login=token[0:9]
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    print(message)
-    if "Ассистент, тест" in message.text:
-            bot.send_message(me,'TESTED!')
+    print(message.text)
     for n in twoch:
-        print(twoch[n])
-        if not(message.chat.id == twoch[n]):
+        if (message.chat.id == twoch[n]) or (message.from_user.id == twoch[n]):
+            print(twoch[n])
+            break
+        else:
             bot.send_message(message.chat.id,'Contact @Kylmakalle first!')
             bot.leave_chat(message.chat.id)
+    if message.chat.type == 'private':
+        print(login)
+        if "/add2ch " + str(login) in message.text:
+            bot.reply_to(message, 'Добавление id в WHITELIST, введите ID')
+            def handle_text(message):
+                print('hi')
+
+    if "Ассистент, тест" in message.text:
+            bot.send_message(me,'TESTED!')
+
 
 @bot.message_handler(content_types=['photo'])
 def handle_photos(message):
@@ -42,6 +59,4 @@ def handle_photos(message):
                         bot.send_message(message.chat.id, message.from_user.first_name+'(@' + message.from_user.username + ') '+'BANNED!')
                         bot.forward_message(me,message.chat.id,message.message_id)
                         bot.send_message(me, message.from_user.first_name + ' (@' + message.from_user.username + ') ' +'id: #'+ str(message.from_user.id) +' BANNED!')
-
-
 bot.polling(none_stop=True, interval=0)
